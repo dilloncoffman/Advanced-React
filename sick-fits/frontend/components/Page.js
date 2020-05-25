9a6b422ab715fcc49d762433e8c21e8804c77091
Dillon Coffman <tug40394@temple.edu>
2020-05-25 12:22:06 -0400

    Added global styles using styled-components injectGlobal; reset spacings and did font size, family, and weight for typography

--- sick-fits/frontend/components/Page.js
+++ sick-fits/frontend/components/Page.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
+import styled, { ThemeProvider, injectGlobal } from 'styled-components'
 import Header from './Header'
 import Meta from './Meta'
-import styled, { ThemeProvider, injectGlobal } from 'styled-components'
 
 // object full of property values to be reused throughout app
 const theme = {
@@ -24,6 +24,33 @@ const Inner = styled.div`
   margin: 0 auto;
   padding: 2rem;
   background: red;
+`
+
+injectGlobal`
+  @font-face {
+    font-family: 'radnika-next';
+    src: url('/static/radnikanext-medium-webfont.woff2') format('woff2');
+    font-weight: normal;
+    font-style: normal;
+  }
+  html {
+    box-sizing: border-box;
+    font-size: 10px;
+  }
+  *, *:before, *:after {
+    box-sizing: inherit;
+  }
+  body {
+    padding: 0;
+    margin: 0;
+    font-size: 1.5rem;
+    line-height: 2;
+    font-family: 'radnika-next';
+  }
+  a {
+    text-decoration: none;
+    color: ${theme.black};
+  }
 `
 
 export default class Page extends Component {
